f3bdb58d2d26156f2e4badc54b9bc7c4d2a53c2b
Dragon Lore <tate.j.foster@gmail.com>
2022-12-05 10:58:05 -0700

    setting up NavBar and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar.js";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
+			<Footer />
 		</div>
 	);
 }
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,19 +1,23 @@
 import "./NavBar.css";
 
 export default function NavBar() {
-	<div className="NavBar-container">
-		<h1>THank Tuesday</h1>
-		<section>
-			<div className="CategoryList">
-				Categories
-				<select>
-					<option>Lawn & Healthcare</option>
-					<option>Life Lessons</option>
-				</select>
-			</div>
-			<div className="searchBar">
-				<input type="text"></input>
-			</div>
-		</section>
-	</div>;
+	return (
+		<div className="NavBarContainer">
+			<h1>Thank Tuesday</h1>
+			<section className="OptionBar">
+				<div className="CategoryList">
+					Categories:
+					<select>
+						<option>Lawn & Healthcare</option>
+						<option>Life Lessons</option>
+						<option>Placehoder</option>
+					</select>
+				</div>
+				<div className="searchBar">
+					Search:
+					<input type="text"></input>
+				</div>
+			</section>
+		</div>
+	);
 }
