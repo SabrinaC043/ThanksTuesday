e50a25b9a8c1021f4dd10262f4a5933585f09c03
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-12-05 20:46:59 -0600

    Department model

--- server/server.js
+++ server/server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log('listening on PORT ${http://localhost:3000/}')
+    console.log('listening on PORT ${http://localhost:3001/}')
 })
