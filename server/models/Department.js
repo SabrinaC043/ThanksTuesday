84001f985ffa71f1e0de10df9cbed35f7c9e0c51
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-12-13 12:51:19 -0600

    fixing dataset

--- server/models/Department.js
+++ server/models/Department.js
@@ -1,42 +1,36 @@
-const { Sequelize, DataTypes, INTEGER, STRING } = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
 
+class Department extends Model { }
 
-sequelize.define('Department', {
-    // model attributes are defined here 
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    role: {
-        type: DataTypes.STRING,
-        allowNull: false,
 
-    },
-    coverIMG: {
-        type: DataTypes.STRING,
-        allowNull: false,
+Department.init(
+    {
+        // model attributes are defined here 
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        coverIMG: {
+            type: DataTypes.STRING,
+            allowNull: false,
 
+        },
     },
-    productId: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Department",
     },
-
-    modelName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-})
+);
 
 
 module.exports = Department;
