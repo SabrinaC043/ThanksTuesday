1c9d907384971601931fbf03ccd1e82f0c54e0b3
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-12-14 11:39:12 -0600

    product data creation

---
+++ server/models/Product.js
@@ -0,0 +1,48 @@
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+
+class Product extends Model { }
+
+Product.init(
+    {
+        productName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+
+        },
+        productPurchasePrice: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+
+        },
+        productSellPrice: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        coverIMG: {
+            type: DataTypes.STRING,
+            allowNull: true,
+
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "product"
+
+    }
+);
+module.exports = Product;
