178376d8cee14cb55a26526881db7955984181dc
Dragon Lore <tate.j.foster@gmail.com>
2022-12-14 10:40:17 -0700

    putting in placeholder shells on each page

---
+++ client/src/pages/Category/Category.js
@@ -0,0 +1,9 @@
+import "./Category.css";
+
+export default function Category() {
+	return (
+		<div className="categoryPage">
+			<h1>Category Page Placeholder</h1>
+		</div>
+	);
+}
---
+++ client/src/pages/Contact/Contact.js
@@ -0,0 +1,9 @@
+import "./Contact.css";
+
+export default function HomeContact() {
+	return (
+		<div className="contactPage">
+			<h1>Contact Page Placeholder</h1>
+		</div>
+	);
+}
